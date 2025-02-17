ae747a8937550f6cdcaf762063817450662f2ae8
Tu Nombre <andresyalca@gmail.com>
2025-02-16 19:41:43 -0500

    creamos modelo

---
+++ src/main/java/com/kamilo/hibernate/modelo/Empleado.java
@@ -0,0 +1,11 @@
+package com.kamilo.hibernate.modelo;
+
+import java.util.Date;
+
+public class Empleado {
+
+  private Long codigo;
+  private String nombre;
+  private String apellido;
+  private Date fechaNacimiento;
+}
