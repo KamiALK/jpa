f9b1f38d17d815acfc107ddc3fad6e17d8a41052
Tu Nombre <andresyalca@gmail.com>
2025-02-16 22:30:10 -0500

    en from Empleado se espera que se le de el nombre de la entidad no el nombre de la tabla EMPLEADO
    ste cambio se debe a que JPA utiliza el nombre de la clase de la entidad
    (Empleado), no el nombre de la tabla (EMPLEADO), en las consultas JPQL.
    La anotación @Table(name = "EMPLEADO") es solo para mapear la clase
    Empleado a la tabla EMPLEADO en la base de datos, pero la consulta debe
    utilizar el nombre de la entidad.

---
+++ src/main/java/com/kamilo/hibernate/tests/TestEmpleados.java
@@ -0,0 +1,39 @@
+package com.kamilo.hibernate.tests;
+
+import java.util.List;
+
+import com.kamilo.hibernate.modelo.Empleado;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+// import jakarta.persistence.PersistenceUnit;
+
+/* vamos 
+ manager.persist() para crear un nuevo registro
+ manager.merge() para modificar un registro
+ manager.remove() para eliminar un registro
+ manager.find() para buscar un registro
+ manager.createQuery() para hacer consultas
+ manager.getTransaction().commit() para confirmar la transacción
+ manager.getTransaction().begin() para iniciar una transacción
+ manager.close() para cerrar la conexión
+ */
+public class TestEmpleados {
+
+  private static EntityManager manager;
+  private static EntityManagerFactory emf;
+
+  public static void main(String[] args) {
+    /*
+     * Creamos el objeto de persistencia
+     */
+    emf = Persistence.createEntityManagerFactory("persistencia");
+    manager = emf.createEntityManager();
+
+    List<Empleado> empleados = (List<Empleado>) manager.createQuery("from  Empleado").getResultList();
+    System.out.println("En esta base de datos hay " + empleados.size() + " empleados");
+
+  }
+
+}
