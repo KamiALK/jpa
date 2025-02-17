eb1aa79a9570ff8deb23b96e00740a93bd85d7da
Tu Nombre <andresyalca@gmail.com>
2025-02-16 19:46:07 -0500

    construimos getters and setters

--- src/main/java/com/kamilo/hibernate/modelo/Empleado.java
+++ src/main/java/com/kamilo/hibernate/modelo/Empleado.java
@@ -12,4 +12,43 @@ public class Empleado {
   // primero creamos un constructor vacio
   public Empleado() {
   }
+
+  public Empleado(Long codigo, String nombre, String apellido, Date fechaNacimiento) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.fechaNacimiento = fechaNacimiento;
+  }
+
+  public Long getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(Long codigo) {
+    this.codigo = codigo;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public Date getFechaNacimiento() {
+    return fechaNacimiento;
+  }
+
+  public void setFechaNacimiento(Date fechaNacimiento) {
+    this.fechaNacimiento = fechaNacimiento;
+  }
 }
