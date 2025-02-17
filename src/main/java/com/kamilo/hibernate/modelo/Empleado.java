2274d2582b70e7015873a011ff6bd3648961c963
Tu Nombre <andresyalca@gmail.com>
2025-02-16 20:22:11 -0500

    definicion de entidades

--- src/main/java/com/kamilo/hibernate/modelo/Empleado.java
+++ src/main/java/com/kamilo/hibernate/modelo/Empleado.java
@@ -1,12 +1,47 @@
 package com.kamilo.hibernate.modelo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Empleado {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+/*
+ * 1.Reconocemos como entidad al agregar @Entity
+ * 2. Indicarle como se debe guardar el la base de datos @Table
+ * 3. Indicarle las columnas que tiene la tabla @Column
+ * columnDefinition = "VARCHAR(100)" se puede agregar para definir el tipo de dato
+ * insertable = false, updatable = false` se puede agregar para que no se pueda insertar ni actualizar
+ * length = 100 se puede agregar para definir la longitud
+ * nullable = false se puede agregar para que no se pueda insertar nulo
+ * precision = 10, scale = 2 se puede agregar para definir la precision y la escala
+ * scale = 2 se puede agregar para definir la escala
+ * table = "EMPLEADO" se puede agregar para definir la tabla
+ * unique = true se puede agregar para definir que el campo es unico
+ * updatable = false se puede agregar para que no se pueda actualizar
+ * Todas las columnas deben tener un identificador
+ *
+ * 
+ */
+@Entity
+@Table(name = "EMPLEADO")
+public class Empleado implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  // para generar el id automaticamente
+  // @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CODIGO_EMPLEADO")
   private Long codigo;
+  @Column(name = "NOMBRE")
   private String nombre;
+  @Column(name = "APELLIDO")
   private String apellido;
+  @Column(name = "FECHA_NACIMIENTO")
   private Date fechaNacimiento;
 
   // primero creamos un constructor vacio
